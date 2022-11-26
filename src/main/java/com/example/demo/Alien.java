484491d36b3cacc01671fcdef186c42ce89b2d5f
Sam VanFossen <samvanf@gmail.com>
2022-11-26 12:33:57 -0500

    Adding laptop class Learning to use Autowired

--- src/main/java/com/example/demo/Alien.java
+++ src/main/java/com/example/demo/Alien.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Alien {
+	// We are NOT creating the object lap, it is being managed by Spring framework.
+	@Autowired
+	Laptop lap;
+	
 	public void code() {
-		System.out.println("Coding...");
+		lap.compile();
 	}
 }
